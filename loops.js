7840c66f0e163763786afa9bfaee86c16948a63e
Ethan <ethansagin@gmail.com>
2019-01-29 15:39:34 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,17 @@
+const arrayReal = [];
+function forLoop(array) {
+  for(let i = 0; i < 25; i++) {
+    if(i === 1) {
+      array.push(`I am ${1} strange loop.`)
+    } else {
+    array.push(`I am ${i} strange loops.`);
+  }}
+  return array
+}
+console.log(forLoop(arrayReal))
+
+function whileLoop(n) {
+  while(n > 0) {
+    return(number--)
+  }
+}
